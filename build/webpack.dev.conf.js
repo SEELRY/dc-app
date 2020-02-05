2b275cf1e1483b877fc3878eea9c350c6bdf1b79
Arthur Wang <wang@xunnn.com>
2020-02-05 23:18:07 +0800

    引入接口文件 引入样式和字体文件

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -13,6 +13,16 @@ const portfinder = require('portfinder')
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
 
+
+//配置接口
+const express = require("express")
+const app = express()
+
+//读取json数据
+var goods = require("../data/goods.json")
+var ratings = require("../data/goods.json")
+var seller = require("../data/seller.json")
+
 const devWebpackConfig = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
@@ -42,6 +52,17 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     quiet: true, // necessary for FriendlyErrorsPlugin
     watchOptions: {
       poll: config.dev.poll,
+    },
+    before(app){
+      app.get("/api/goods",(req,res) => {
+        res.json(goods)
+      })
+      app.get("/api/ratings",(req,res) => {
+        res.json(ratings)
+      })
+      app.get("/api/seller",(req,res) => {
+        res.json(seller)
+      })
     }
   },
   plugins: [
