69bbe94c8e2adc04e09fca261abc31a408ae2d4d
Arthur Wang <wang@xunnn.com>
2020-02-05 23:44:30 +0800

    添加组件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
   <div id="app">
+    <!-- header -->
+    <app-header></app-header>
+
+    <!-- nav -->
+
+    <!-- content -->
+
     
   </div>
 </template>
 
 <script>
-
+import Header from './components/header/Header';
+export default {
+  name: 'App',
+  components: {
+    "app-header":Header
+  }
+}
 </script>
 
 <style>
