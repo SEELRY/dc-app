023e364b96278543047aa28e17d76f714e77ad7a
Arthur Wang <wang@xunnn.com>
2020-02-16 13:56:58 +0800

    添加右侧商品列表并绑定数据和样式 添加滚动插件

--- src/components/goods/Goods.vue
+++ src/components/goods/Goods.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="goods">
         <!-- 分类列表 -->
-        <div class="menu-wrapper">
+        <div class="menu-wrapper" ref="menuScroll">
             <ul>
                 <!-- 专场 -->
                 <li class="menu-item">
@@ -20,16 +20,59 @@
             </ul>
         </div>
         <!-- 商品列表 -->
-        <div class="foods-wrapper"></div>
+        <div class="foods-wrapper" ref="foodScroll">
+            <ul>
+                <!-- 专场 -->
+                <li class="container-list">
+                    <div v-for="(item,index) in container.operation_source_list" :key="index">
+                        <img :src="item.pic_url" alt="">
+                    </div>
+                </li>
+
+                <!-- 具体分类 -->
+                <li class="food-list" v-for="(item,index) in goods" :key="index">
+                    <h3 class="title">{{item.name}}</h3>
+                    <!-- 具体商品列表 -->
+                    <ul>
+                        <li class="food-item" v-for="(item,index) in item.spus" :key="index">
+                            <div class="icon" :style="head_bg(item.picture)"></div>
+                            <div class="content">
+                                <h3 class="name">{{item.name}}</h3>
+                                <p class="disc" v-if="item.description">{{item.description}}</p>
+                                <div class="extra">
+                                    <span class="saled">{{item.month_saled_content}}</span>
+                                    <span class="saled">{{item.praise_content}}</span>
+                                </div>
+                                <img class="product" :src="item.product_label_picture" alt="">
+                                <p class="price">
+                                    <span class="text">&yen;{{item.min_price}}</span>
+                                    <span class="unit">/{{item.unit}}</span>
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
+import BScroll from "better-scroll";
 export default {
     data(){
         return{
             container:{},
             goods:[]
+        }
+    },
+    methods:{
+        head_bg(imgName){
+            return "background-image:url(" + imgName + ");"
+        },
+        initScroll(){
+            new BScroll(this.$refs.menuScroll)
+            new BScroll(this.$refs.foodScroll)
         }
     },
     created(){
@@ -44,6 +87,9 @@ export default {
             this.goods = response.data.food_spu_tags
             // console.log(this.container)
             // console.log(this.goods)
+            
+            //执行滚动方法
+            this.initScroll()
             }
         })
     }
@@ -86,6 +132,119 @@ export default {
     width: 15px;
     height: 15px;
     vertical-align: middle;
+}
+
+/* 专场样式 */
+.goods .foods-wrapper .container-list{
+    padding: 11px 11px 0 11px;
+    border-bottom: 1px solid #E4E4E4;
+}
+.goods .foods-wrapper .container-list img{
+    width: 100%;
+    margin-bottom: 11px;
+    border-radius: 5px;
+}
+/* 分类下的商品布局 */
+.goods .foods-wrapper .food-list{
+	padding: 11px;
+}
+.goods .foods-wrapper .food-list .title{
+	height: 13px;
+	font-size: 13px;
+	background: url(./img/btn_yellow_highlighted@2x.png) no-repeat left center;
+	background-size: 2px 10px;
+	padding-left: 7px;
+	margin-bottom: 12px;
+}
+.goods .foods-wrapper .food-list .food-item{
+	display: flex;
+	margin-bottom: 25px;
+  position: relative;
+}
+.goods .foods-wrapper .food-list .food-item  .icon{
+	flex: 0 0 63px;
+	background-position: center;
+	background-size: 120% 100%;background-repeat: no-repeat;
+	margin-right: 11px;
+	height: 75px;
+}
+.goods .foods-wrapper .food-list .food-item .content{
+	flex: 1;
+}
+/* 具体内容样式 */ 
+.goods .foods-wrapper .food-list .food-item .content .name{
+	font-size: 16px;
+	line-height: 21px;
+	color: #333333;
+	margin-bottom: 10px;
+	padding-right: 27px;
+}
+
+.goods .foods-wrapper .food-list .food-item .content .desc{
+	font-size: 10px;
+	line-height: 19px;
+	color: #bfbfbf;
+	margin-bottom: 8px;
+	
+	/* 超出部分显示省略号*/
+	-webkit-line-clamp: 1;
+	display: -webkit-box;
+	-webkit-box-orient: vertical;
+	overflow: hidden;
+}
+
+.goods .foods-wrapper .food-list .food-item .content .extra{
+	font-size: 10px;
+	color: #BFBFBF;
+	margin-bottom: 7px;
+}
+.goods .foods-wrapper .food-list .food-item .content .extra .saled{
+	margin-right: 14px;
+}
+.goods .foods-wrapper .food-list .food-item .content .product{
+	height: 15px;
+	margin-bottom: 6px;
+}
+.goods .foods-wrapper .food-list .food-item .content .price{
+	font-size: 0;
+}
+.goods .foods-wrapper .food-list .food-item .content .price .text{
+	font-size: 14px;
+	color: #fb4e44;
+}
+.goods .foods-wrapper .food-list .food-item .content .price .unit{
+	font-size: 12px;
+	color: #BFBFBF;
+}
+
+/* 当前选中 */ 
+.goods .menu-wrapper .menu-item.current{
+	background: white;
+	font-weight: bold;
+	margin-top: -1px;
+}
+.goods .menu-wrapper .menu-item:first-child.current{
+	margin-top: 1px;
+}
+
+.goods .foods-wrapper .food-list .food-item .cartcontrol-wrapper{
+	position: absolute;
+	right: 0;
+	bottom: 0;
+}
+
+.goods .menu-wrapper .menu-item .num{
+	position: absolute;
+	right: 5px;
+	top: 5px;
+	width: 13px;
+	height: 13px;
+	border-radius: 50%;
+	color: white;
+	background: red;
+	text-align: center;
+	font-size: 7px;
+	line-height: 13px;
 }
 
 </style>
